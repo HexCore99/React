3326e8e8750ecf08d66470df20227a20639e78cf
heXcore <hexcore@gmail.com>
2025-11-10 03:53:35 +0600

    Add initial project structure and pages for WorldWise application
    - Set up main entry point in `main.jsx` with React StrictMode
    - Create `AppLayout` component for application layout
    - Implement `HomePage` with navigation to the app
    - Add `PageNotFound` component for handling 404 errors
    - Create `Pricing` page with navigation
    - Add `Product` page for product details
    - Configure Vite for React project

---
+++ worldWise/src/pages/Pricing.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../components/PageNav";
+function Priciing() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Pricing</h1>
+    </div>
+  );
+}
+
+export default Priciing;
